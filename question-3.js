69c7625b0efd8a2c9dded3cae36b1bf39aa40655
Mon <tracer.coolkid@gmail.com>
2024-07-30 10:12:55 +0700

    algorithm-skill-checkpoint

--- question-3.js
+++ question-3.js
@@ -1,20 +1,20 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: O(n) เพราะ จำนวนรอบการทำงานจะเพิ่มขึ้นตามขนาดข้อมูล Input
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: O(log n) เพราะ จำนวนรอบการทำงานจะเพิ่มขึ้นตามขนาดข้อมูล Input แบบ Logarithm 
 
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
+Answer:binary search มีประสิทธิภาพ มากกว่าเพราะ loop ของการทำงานลดลงครึ่งนึงทุกครั้ง ทำให้จำนวนรอบในการทำงานน้อยกว่า
 
 
 */
--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
+Answer: Big O เป็น O(n²) เพราะว่า จำนวนรอบเพิ่มขึ้นตามกำลังสองของขนาดข้อมูล ใช้ Loop ซ้อนกัน 2 ชั้น
 
 
 */
